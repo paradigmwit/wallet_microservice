b2a0419f19aecf0c617a55a80df8cd8bc81cac70
Fahd Khan <fahd_khan_01@hotmail.com>
2020-05-26 17:06:42 +0200

    Postgres

--- src/main/java/com/leovegas/wallet/controller/PlayerController.java
+++ src/main/java/com/leovegas/wallet/controller/PlayerController.java
@@ -5,16 +5,12 @@ import com.leovegas.wallet.exceptions.PlayerDoesntExistException;
 import com.leovegas.wallet.service.PlayerService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
